3c0d811ca72eb3cd6d7b4c23aa28f748b57c4681
Sean Carter <scarter0313@gmail.com>
2019-01-21 00:03:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObject = {}
   newObject[key] = value
-  return Object.assign(key, newObject)
+  return Object.assign({}, object, newObject)
 }
 
 // function updateObjectWithKeyAndValue(object, key, value) {
