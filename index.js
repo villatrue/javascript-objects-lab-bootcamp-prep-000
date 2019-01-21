a73830e47381be53137d312bd22557e3a9efb682
Sean Carter <scarter0313@gmail.com>
2019-01-21 00:11:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
 }
